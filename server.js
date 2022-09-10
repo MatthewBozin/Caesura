05232e115f342bd2030466ecaba10c913f242d9d
Matthew Bozin <mbozin94@gmail.com>
2022-09-10 10:36:46 -0700

    Removed vestigial files

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ connectDB()
 
 app.set('view engine', 'ejs')
 app.use(cors())
-app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))

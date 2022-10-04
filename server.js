56db80d48a11ba965c1700d386735b02fbf3c060
Matthew Bozin <mbozin94@gmail.com>
2022-10-04 14:01:18 -0700

    Testing heroku buildpack

--- server.js
+++ server.js
@@ -62,10 +62,10 @@ app.use('/poems', poemRoutes)
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/src')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/src', 'index.js'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
  

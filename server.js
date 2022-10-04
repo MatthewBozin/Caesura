06e4a354ca5fa9f2e521f17a54e647b254f0fedf
Matthew Bozin <mbozin94@gmail.com>
2022-10-04 13:43:37 -0700

    Removed build from frontend server script in server.js

--- server.js
+++ server.js
@@ -62,10 +62,10 @@ app.use('/poems', poemRoutes)
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/src')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/src', 'index.html'));
   });
 }
  

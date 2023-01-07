1db6c873f0620fc8d8ffc29d17d31c80f9607159
Matthew Bozin <mbozin94@gmail.com>
2023-01-07 13:03:40 -0800

    Converted dataService and Choice.tsx to Typescript. Create types.js in types folder to contain app types. Added jsx to tsconfig.

--- client/src/components/Choice.tsx
+++ client/src/components/Choice.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import {Typography} from '@mui/material'
+import { Typography } from '@mui/material'
 import { motion } from 'framer-motion'
 
-const Choice = (props) => {
+const Choice = (props: any) => {
   return (
     <motion.div className="choice" initial={{ scale: 0.9 }} whileHover={{ scale: 1 }}>
-      <div onClick={() => {props.add(props.poem.line, props.poem.author, props.poem.title)}}>
+      <div onClick={() => { props.add(props.poem.line, props.poem.author, props.poem.title) }}>
         <Typography>from</Typography>
         <Typography variant={'h4'}>{props.poem.title}</Typography>
         <Typography variant={'h6'}>by {props.poem.author}</Typography>
-        {props.poem.prevLines.map((prevLine, index) => {
+        {props.poem.prevLines.map((prevLine: string, index: number) => {
           return <Typography className="prevLine" key={index}>{prevLine}</Typography>
         })}
         <Typography>{props.poem.line}</Typography>
--- client/src/dataService.ts
+++ client/src/dataService.ts
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const URL = axios.create({
     withCredentials: true,
-    headers: {"Content-type": "application/json"}
+    headers: { "Content-type": "application/json" }
 })
 
 class DataService {
-    signup(msg) {
+    signup(msg: any) {
         return URL.post(`/signup`, msg)
     }
     checkLogin() {
         return URL.get(`/login`)
     }
-    login(msg) {
+    login(msg: any) {
         return URL.post(`/login`, msg)
     }
     logout() {
@@ -24,28 +24,28 @@ class DataService {
     getFeed() {
         return URL.get(`/poems/getFeed`)
     }
-    createPoem(msg) {
+    createPoem(msg: any) {
         return URL.post(`/poems/createPoem`, msg)
     }
-    deletePoem(msg) {
+    deletePoem(msg: any) {
         return URL.post(`/poems/deletePoem`, msg)
     }
     getPoemData() {
         return URL.get(`/poems/poemData`)
     }
-    snap(msg) {
+    snap(msg: any) {
         return URL.put(`/poems/snap`, msg)
     }
-    createComment(msg) {
+    createComment(msg: any) {
         return URL.post(`/comments/createComment`, msg)
     }
-    getComments(msg) {
+    getComments(msg: any) {
         return URL.post(`/comments/getComments`, msg)
     }
-    snapComment(msg) {
+    snapComment(msg: any) {
         return URL.put(`/comments/snapComment`, msg)
     }
-    deleteComment(msg) {
+    deleteComment(msg: any) {
         return URL.post(`/comments/deleteComment`, msg)
     }
 }

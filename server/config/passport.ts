f260216ad89582646aa4455bad06ae349880e752
Matthew Bozin <mbozin94@gmail.com>
2023-01-07 12:25:09 -0800

    Convert database and passport files in config to typescript.

--- server/config/passport.ts
+++ server/config/passport.ts
@@ -1,10 +1,9 @@
 const LocalStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
 const User = require('../models/User')
 
-module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+module.exports = function (passport: any) {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email: string, password: string, done: any) => {
+    User.findOne({ email: email.toLowerCase() }, (err: any, user: any) => {
       if (err) { return done(err) }
       if (!user) {
         return done(null, false, { msg: `Email ${email} not found.` })
@@ -12,7 +11,7 @@ module.exports = function (passport) {
       if (!user.password) {
         return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
       }
-      user.comparePassword(password, (err, isMatch) => {
+      user.comparePassword(password, (err: any, isMatch: boolean) => {
         if (err) { return done(err) }
         if (isMatch) {
           return done(null, user)
@@ -21,13 +20,13 @@ module.exports = function (passport) {
       })
     })
   }))
-  
 
-  passport.serializeUser((user, done) => {
+
+  passport.serializeUser((user: any, done: any) => {
     done(null, user.id)
   })
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
+  passport.deserializeUser((id: any, done: any) => {
+    User.findById(id, (err: any, user: any) => done(err, user))
   })
 }
